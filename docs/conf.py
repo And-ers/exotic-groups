cba17273345f3cde2bbefa98a6a254ae26f09b06
Anders Cornect <accornect@mun.ca>
2023-12-24 13:12:49 -0330

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+import os
+import sphinx_rtd_theme
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
